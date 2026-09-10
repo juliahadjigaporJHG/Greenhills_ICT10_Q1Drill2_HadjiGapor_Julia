308be5350b97b773ce801482a702ae94d4bf883a
juliahadjigaporJHG <hadjigapor.jd@obmontessori.edu.ph>
2026-09-10 21:33:45 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# PyScript
+from pyscript import display
+
+
+first_name = 'Julia'
+last_name = 'Hadji Gapor'
+
+display(first_name  + last_name, target='result')
+
+string_sample = 'Hello World'
+integer_sample = 14
+float_sample = 14
+bollean_sample = True
+list_sample = [1, 2, 3, 4]
+tuple_sample = [5, 6, 7]
+set_sample = {'red', 'blue', 'green'}
+dict_sample = {'champion' : 'eala', '1st runner up' : 'alexa'}
+
+_section = '10 Topaz'
+class_adviser = 'Mr. De Guzman'
+year = 2026
+value_of_pie = 3.14
+
+
+display(f'Hi!  Im from {_section}, My class adviser is {class_adviser}. The current school year is {year}, Fun fact: The value of pie is {value_of_pie}', target='result')
+
+
+x = 5
+y = 10
+z = 15
+
+display(x + y * z, target='result')
+
